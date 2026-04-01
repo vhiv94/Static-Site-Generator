2576d2ce05e39140ae3eb9235060bb8f0711236a
vhiv94 <vhiv94@gmail.com>
2026-04-01 00:25:40 -0500

    Add TextNode

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
+from textnode import TextNode, TextType
+
+
 def main() -> None:
-    print("Hello, World!")
+    test_node = TextNode("This is some anchor text", TextType.ANCHOR_TEXT, "https://www.boot.dev")
+    print(test_node)
 
 if __name__ == "__main__":
     main()
---
+++ src/textnode.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+from enum import Enum
+
+
+class TextType(Enum):
+    PLAIN_TEXT = "plain"
+    BOLD_TEXT = "bold"
+    ITALIC_TEXT = "italic"
+    CODE_TEXT = "code snippet"
+    ANCHOR_TEXT = "hyperlink"
+    ALT_TEXT = "image link"
+
+class TextNode:
+    def __init__(self, text: str, text_type: TextType, url: str) -> None:
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other: TextNode) -> bool:
+        return self.text == other.text\
+        and self.text_type == other.text_type\
+        and self.url == other.url
+    
+    def __repr__(self) -> str:
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
