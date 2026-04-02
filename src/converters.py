07662ebcb2225dc5a1cd2890b49bd4475341cf06
vhiv94 <vhiv94@gmail.com>
2026-04-02 00:32:40 -0500

    Add split nodes on delimiter

--- src/converters.py
+++ src/converters.py
@@ -1,5 +1,5 @@
 from text_node import TextNode, TextType
-from parent_node import ParentNode
+# from parent_node import ParentNode
 from leaf_node import LeafNode
 
 
@@ -23,3 +23,21 @@ def text_node_to_leaf_node(text_node: TextNode) -> LeafNode:
             raise ValueError("Error: text node to leaf node: invalid path to image")
         
     return LeafNode(text_node.text_type.value, text_node.text)
+
+def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: TextType) -> list[TextNode]:
+    new_nodes: list[TextNode] = []
+    for node in old_nodes:
+        if node.text.find(delimiter) == -1:
+            new_nodes.append(node)
+            continue
+        split_node: list = [(x, node.text_type if i%2==0 else text_type) for i,x in enumerate(node.text.split(delimiter))]
+        if split_node[0][0] == "":
+            del split_node[0]
+        new_nodes.extend(list(map(lambda elem: TextNode(elem[0], elem[1]), split_node)))
+ 
+    return new_nodes
+
+node = TextNode("_This_ is **bold** text with a `code block` word", TextType.PLAIN_TEXT)
+new_nodes = split_nodes_delimiter([node], "`", TextType.CODE_TEXT)
+new_nodes2 = split_nodes_delimiter(new_nodes, "**", TextType.BOLD_TEXT)
+new_nodes3 = split_nodes_delimiter(new_nodes2, "_", TextType.ITALIC_TEXT)
